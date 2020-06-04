168b189daaf624b054a9d13444f17459cfd7c372
Garrett Singer <gesinger@gmail.com>
2020-06-04 10:07:14 -0400

    Add switch for showing only segments with timing info

--- src/SegmentsPanel.js
+++ src/SegmentsPanel.js
@@ -3,6 +3,9 @@ import SegmentsList from './SegmentsList';
 import SegmentRequestInfo from './SegmentRequestInfo';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import Box from '@material-ui/core/Box';
@@ -10,6 +13,7 @@ import Box from '@material-ui/core/Box';
 export default function SegmentsPanel(props) {
   const { player } = props;
   const [selectedPlaylistType, setSelectedPlaylistType] = useState('Main');
+  const [showOnlyLoaded, setShowOnlyLoaded] = useState(false);
   const [selectedSegmentIndex, setSelectedSegmentIndex] = useState(null);
 
   const handlePlaylistSelection = (event) => {
@@ -21,27 +25,43 @@ export default function SegmentsPanel(props) {
   };
 
   const selectedPlaylist = player[`${selectedPlaylistType.toLowerCase()}Playlist`];
+  const segmentsWithTimes = selectedPlaylist.segments.filter(
+    (segment) => segment.start && segment.end
+  );
 
   return (
     <div>
-      {player.audioPlaylist && (
-        <Box ml={1} mt={1}>
-          <FormControl>
-            <InputLabel>Playlist</InputLabel>
-            <Select
-              value={selectedPlaylistType}
-              onChange={handlePlaylistSelection}
-            >
-              <MenuItem value='Main'>Main</MenuItem>
-              <MenuItem value='Audio'>Audio</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      )}
+      <Box ml={1} mt={1}>
+        <FormGroup row>
+          {player.audioPlaylist && (
+            <Box mr={3}>
+              <FormControl>
+                <InputLabel>Playlist</InputLabel>
+                <Select
+                  value={selectedPlaylistType}
+                  onChange={handlePlaylistSelection}
+                >
+                  <MenuItem value='Main'>Main</MenuItem>
+                  <MenuItem value='Audio'>Audio</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          )}
+          <FormControlLabel
+            control={
+              <Switch
+                checked={showOnlyLoaded}
+                onChange={(event) => setShowOnlyLoaded(event.target.checked)}
+              />
+            }
+            label="Show only segments with timing info"
+          />
+        </FormGroup>
+      </Box>
       <Box display="flex" flexDirection="row">
         <Box>
           <SegmentsList
-            segments={selectedPlaylist.segments}
+            segments={showOnlyLoaded ? segmentsWithTimes : selectedPlaylist.segments}
             setSegmentSelection={setSegmentSelection}
           />
         </Box>
